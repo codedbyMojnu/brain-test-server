3015b5366fc179aa862cb1c0a2e51bd1129469c6
codebymojnu <thisismojnu@gmail.com>
2025-06-29 01:50:16 +0600

    all works only options needed to fix

--- models/Level.js
+++ models/Level.js
@@ -9,25 +9,16 @@ const levelSchema = new mongoose.Schema(
         },
         options: {
             type: [String], // Defines an array of strings
-            required: true,
+            trim: true,
         },
         hint: {
-            type: [String], // Defines an array of strings
+            type: String, // Defines an array of strings
             required: true,
         },
         answer: {
             type: String,
             required: [true, 'An answer is required.'],
             trim: true,
-            // It's a best practice to ensure the answer is one of the provided options
-            validate: {
-                validator: function (value) {
-                    // 'this' refers to the document being saved.
-                    // The validator checks if the provided answer exists in the options array.
-                    return this.options.includes(value);
-                },
-                message: 'The answer must be one of the provided options.',
-            },
         },
         explanation: {
             type: String,
